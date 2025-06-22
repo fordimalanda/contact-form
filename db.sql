3ef2414ad95ac9265395b11be57ac60030c22e43
fordimalanda <fordimalanda7@gmail.com>
2025-06-22 12:17:08 +0100

    Creation de la table 'contacts'

--- db.sql
+++ db.sql
@@ -3,3 +3,13 @@ DROP DATABASE IF EXISTS `contact`;
 CREATE DATABASE IF NOT EXISTS `contact`;
 
 USE `contact`;
+
+CREATE TABLE contacts (
+    id_contact INT AUTO_INCREMENT,
+    nom VARCHAR(100),
+    email VARCHAR(255),
+    message TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY(`id_contact`)
+);
